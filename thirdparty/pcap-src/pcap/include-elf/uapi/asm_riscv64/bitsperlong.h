a98732ec29b0a4d396a7cc5438b3bc4ff05101b8
Tom Herbert <tom@herbertland.com>
2025-04-02 14:50:22 -0700

    libpcap_xdp2: Pruned version libpcap static libraries
    This is to build a pruned version of pcap library called as
    libpcap_xdp2*.a for static linking parser and parser threads tests
    and also for running these tests under spike.
    
    Please refer to documentation/xdp2-pcap.md for some
    more information and build instructions.
    
    Signed off: Deva Ranganathan<none>
    
    Signed-off-by: Tom Herbert <tom@herbertland.com>
    Signed-off-by: Tom Herbert <tom@sipanda.io>

---
+++ thirdparty/pcap-src/pcap/include-elf/uapi/asm_riscv64/bitsperlong.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only WITH Linux-syscall-note */
+/*
+ * Copyright (C) 2012 ARM Ltd.
+ * Copyright (C) 2015 Regents of the University of California
+ */
+
+#ifndef _ASM_RISCV_BITSPERLONG_H
+#define _ASM_RISCV_BITSPERLONG_H
+
+#include <asm-generic/bitsperlong.h>
+
+#endif /* _ASM_RISCV_BITSPERLONG_H */
---
+++ thirdparty/pcap-src/pcap/include-elf/uapi/features_elf.h
@@ -0,0 +1,2 @@
+//#define __USE_XOPEN2K
+#include <sys/features_elf.h>
