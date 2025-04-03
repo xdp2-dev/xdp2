5d17684f567923035c0e0c077f9d3193e0f9b796
Tom Herbert <tom@herbertland.com>
2025-04-03 16:21:07 -0700

    proto_def: IP overlay protocol definition
    proto_defs/proto_ip.h contains the definition for parsing IP to
    distinguish between IPv4 and IPv6 as an overlay
    
    Signed-off-by: Tom Herbert <tom@herbertland.com>
    Signed-off-by: Tom Herbert <tom@sipanda.io>

--- src/include/xdp2/proto_defs/proto_ip.h
+++ src/include/xdp2/proto_defs/proto_ip.h
@@ -24,15 +24,65 @@
  * SUCH DAMAGE.
  */
 
-/* Include for all defined proto nodes */
+#ifndef __XDP2_PROTO_IP_H__
+#define __XDP2_PROTO_IP_H__
 
-/* Don't use header file guard here */
+#include "xdp2/parser.h"
 
-#include "xdp2/proto_defs/proto_arp_rarp.h"
-#include "xdp2/proto_defs/proto_batman.h"
-#include "xdp2/proto_defs/proto_ether.h"
-#include "xdp2/proto_defs/proto_fcoe.h"
-#include "xdp2/proto_defs/proto_gre.h"
-#include "xdp2/proto_defs/proto_icmp.h"
-#include "xdp2/proto_defs/proto_igmp.h"
-#include "xdp2/proto_defs/proto_ip.h"
+/* IP overlay protocol definitions */
+
+#include <asm/byteorder.h>
+
+struct ip_hdr_byte {
+#if defined(__LITTLE_ENDIAN_BITFIELD)
+	__u8    rsvd:4,
+		version:4;
+#elif defined(__BIG_ENDIAN_BITFIELD)
+	__u8    version:4,
+		rsvd:4;
+#else
+#error "Please fix <asm/byteorder.h>"
+#endif
+};
+
+static inline int ip_proto(const void *viph)
+{
+	return ((struct ip_hdr_byte *)viph)->version;
+}
+
+static inline size_t ip_min_len(const void *viph)
+{
+	return sizeof(struct ip_hdr_byte);
+}
+
+static inline int ip_proto_by_key(const void *viph, __u32 key)
+{
+	return key;
+}
+
+#endif /* __XDP2_PROTO_IP_H__ */
+
+#ifdef XDP2_DEFINE_PARSE_NODE
+
+/* parse_ip protocol definition
+ *
+ * Parses first byte of IP header to distinguish IP version (i.e. IPv4
+ * and IPv6)
+ *
+ * Next protocol operation returns IP version number (e.g. 4 for IPv4,
+ * 6 for IPv6)
+ */
+static const struct xdp2_proto_def xdp2_parse_ip __unused() = {
+	.name = "IP overlay",
+	.overlay = 1,
+	.min_len = sizeof(struct ip_hdr_byte),
+	.ops.next_proto = ip_proto,
+};
+
+static const struct xdp2_proto_def xdp2_parse_ip_by_key __unused() = {
+	.name = "IP overlay by key",
+	.overlay = 1,
+	.ops.next_proto_keyin = ip_proto_by_key,
+};
+
+#endif /* XDP2_DEFINE_PARSE_NODE */
