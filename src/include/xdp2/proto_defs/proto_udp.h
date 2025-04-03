c66bb2d015de5b59fad5ab30975fb9079edad732
Tom Herbert <tom@herbertland.com>
2025-04-03 16:26:53 -0700

    proto_def: UDP protocol definition
    proto_defs/proto_udp.h contains the definition for parsing UDP
    
    Signed-off-by: Tom Herbert <tom@herbertland.com>

--- src/include/xdp2/proto_defs/proto_udp.h
+++ src/include/xdp2/proto_defs/proto_udp.h
@@ -24,31 +24,30 @@
  * SUCH DAMAGE.
  */
 
-/* Include for all defined proto nodes */
+#ifndef __XDP2_PROTO_UDP_H__
+#define __XDP2_PROTO_UDP_H__
 
-/* Don't use header file guard here */
+#include <linux/udp.h>
 
-#include "xdp2/proto_defs/proto_arp_rarp.h"
-#include "xdp2/proto_defs/proto_batman.h"
-#include "xdp2/proto_defs/proto_ether.h"
-#include "xdp2/proto_defs/proto_fcoe.h"
-#include "xdp2/proto_defs/proto_gre.h"
-#include "xdp2/proto_defs/proto_icmp.h"
-#include "xdp2/proto_defs/proto_igmp.h"
-#include "xdp2/proto_defs/proto_ip.h"
-#include "xdp2/proto_defs/proto_ipv4.h"
-#include "xdp2/proto_defs/proto_ipv4ip.h"
-#include "xdp2/proto_defs/proto_ipv6.h"
-#include "xdp2/proto_defs/proto_ipv6_eh.h"
-#include "xdp2/proto_defs/proto_ipv6ip.h"
-#include "xdp2/proto_defs/proto_ipv6_nd.h"
-#include "xdp2/proto_defs/proto_l2tp.h"
-#include "xdp2/proto_defs/proto_l2tp_v0.h"
-#include "xdp2/proto_defs/proto_mpls.h"
-#include "xdp2/proto_defs/proto_ports.h"
-#include "xdp2/proto_defs/proto_ppp.h"
-#include "xdp2/proto_defs/proto_pppoe.h"
-#include "xdp2/proto_defs/proto_protobuf.h"
-#include "xdp2/proto_defs/proto_tcp.h"
-#include "xdp2/proto_defs/proto_tipc.h"
-#include "xdp2/proto_defs/proto_udp.h"
+/* Generic UDP protocol definitions */
+
+static inline int udp_proto(const void *vudph)
+{
+	return ((struct udphdr *)vudph)->dest;
+}
+
+#endif /* __XDP2_PROTO_UDP_H__ */
+
+#ifdef XDP2_DEFINE_PARSE_NODE
+
+/* xdp2_parse_udp protocol definition
+ *
+ * Parse UDP header
+ */
+static const struct xdp2_proto_def xdp2_parse_udp __unused() = {
+	.name = "UDP",
+	.min_len = sizeof(struct udphdr),
+	.ops.next_proto = udp_proto,
+};
+
+#endif /* XDP2_DEFINE_PARSE_NODE */
