1d0d8cb69871097272d8c915c05d911300daab24
Tom Herbert <tom@herbertland.com>
2025-04-03 16:27:10 -0700

    proto_def: VLAN protocol definition
    proto_defs/proto_vlan.h contains the definition for parsing VLAN
    
    Signed-off-by: Tom Herbert <tom@herbertland.com>
    Signed-off-by: Tom Herbert <tom@sipanda.io>

--- src/include/xdp2/proto_defs/proto_vlan.h
+++ src/include/xdp2/proto_defs/proto_vlan.h
@@ -24,32 +24,41 @@
  * SUCH DAMAGE.
  */
 
-/* Include for all defined proto nodes */
+#ifndef __XDP2_PROTO_VLAN_H__
+#define __XDP2_PROTO_VLAN_H__
 
-/* Don't use header file guard here */
+#include "xdp2/parser.h"
 
-#include "xdp2/proto_defs/proto_arp_rarp.h"
-#include "xdp2/proto_defs/proto_batman.h"
-#include "xdp2/proto_defs/proto_ether.h"
-#include "xdp2/proto_defs/proto_fcoe.h"
-#include "xdp2/proto_defs/proto_gre.h"
-#include "xdp2/proto_defs/proto_icmp.h"
-#include "xdp2/proto_defs/proto_igmp.h"
-#include "xdp2/proto_defs/proto_ip.h"
-#include "xdp2/proto_defs/proto_ipv4.h"
-#include "xdp2/proto_defs/proto_ipv4ip.h"
-#include "xdp2/proto_defs/proto_ipv6.h"
-#include "xdp2/proto_defs/proto_ipv6_eh.h"
-#include "xdp2/proto_defs/proto_ipv6ip.h"
-#include "xdp2/proto_defs/proto_ipv6_nd.h"
-#include "xdp2/proto_defs/proto_l2tp.h"
-#include "xdp2/proto_defs/proto_l2tp_v0.h"
-#include "xdp2/proto_defs/proto_mpls.h"
-#include "xdp2/proto_defs/proto_ports.h"
-#include "xdp2/proto_defs/proto_ppp.h"
-#include "xdp2/proto_defs/proto_pppoe.h"
-#include "xdp2/proto_defs/proto_protobuf.h"
-#include "xdp2/proto_defs/proto_tcp.h"
-#include "xdp2/proto_defs/proto_tipc.h"
-#include "xdp2/proto_defs/proto_udp.h"
-#include "xdp2/proto_defs/proto_vlan.h"
+#define VLAN_PRIO_MASK		0xe000 /* Priority Code Point */
+#define VLAN_PRIO_SHIFT		13
+#define VLAN_VID_MASK		0x0fff /* VLAN Identifier */
+
+/* VLAN node protocol definitions */
+
+struct vlan_hdr {
+	__be16  h_vlan_TCI;
+	__be16  h_vlan_encapsulated_proto;
+};
+
+static inline int vlan_proto(const void *vvlan)
+{
+	return ((struct vlan_hdr *)vvlan)->h_vlan_encapsulated_proto;
+}
+
+#endif /* __XDP2_PROTO_VLAN_H__ */
+
+#ifdef XDP2_DEFINE_PARSE_NODE
+
+/* xdp2_parse_vlan protocol definition
+ *
+ * Parse VLAN header
+ *
+ * Next protocol operation returns Ethertype (e.g. ETH_P_IPV4)
+ */
+static const struct xdp2_proto_def xdp2_parse_vlan __unused() = {
+	.name = "VLAN",
+	.min_len = sizeof(struct vlan_hdr),
+	.ops.next_proto = vlan_proto,
+};
+
+#endif /* XDP2_DEFINE_PARSE_NODE */
