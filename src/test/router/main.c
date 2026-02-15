92cb783988c4252b5f4bc86ba34927185ab5d9b8
Tom Herbert <tom@herbertland.com>
2026-02-14 16:20:35 -0800

    test: Remove hdr_off arg from metadata extract/handler funcs in router
    Remove hdr_off argument from metadata extract functions. Call
    xdp2_parse_hdr_offset to get the header offset. Call
    XDP2_CTRL_SET_BASIC_PKT_DATA with new arguments.
    
    Signed-off-by: Tom Herbert <tom@herbertland.com>

--- src/test/router/main.c
+++ src/test/router/main.c
@@ -130,6 +130,7 @@ static void run_router(const struct xdp2_parser *parser, char **pcap_files,
 
 		XDP2_CTRL_RESET_VAR_DATA(&ctrl);
 		XDP2_CTRL_SET_BASIC_PKT_DATA(&ctrl, packets[pn].packet,
+					     packets[pn].packet,
 					     packets[pn].cap_len, i);
 
 		xdp2_parse(parser, packets[pn].packet, packets[pn].cap_len,
